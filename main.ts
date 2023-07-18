b7d3e44c3c49c72f366697bcc6688062c54cde5a
Ubuntu <ubuntu@ip-172-31-25-44.ec2.internal>
2023-07-18 17:21:39 +0000

    2nd commit

--- main.ts
+++ main.ts
@@ -1,13 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { NullProvider, HashiCupsProvider } from '@cdktf/provider-null';
+import { HashiCupsProvider, NullProvider } from '@cdktf/provider-null';
 import { TerraformOutput, TerraformStack, Token } from 'cdktf';
+import * as path from 'path';
+import * as fs from 'fs';
+
+interface OrderItem {
+  coffee: string;
+  quantity: number;
+}
 
 export class CoffeeOrderingStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    const orderItemsPath = './order_items';
+    const ordersPath = './orders';
 
     // HashiCups provider
     const hashicupsProvider = new HashiCupsProvider(this, 'hashicups', {
@@ -19,20 +26,21 @@ export class CoffeeOrderingStack extends TerraformStack {
       alias: 'null',
     });
 
-    // Orders
-    const orderFiles = this.listFiles(orderItemsPath);
-    orderFiles.forEach((file) => {
-      const orderId = file.replace('.txt', '');
-      const orderFilePath = `${orderItemsPath}/${file}`;
+    // Process order folders
+    const orderFolders = this.listOrderFolders(ordersPath);
+    orderFolders.forEach((folderName) => {
+      const orderId = path.basename(folderName);
 
-      // Read the file contents
-      const orderContents = this.readOrderContents(orderFilePath);
+      // Read order items from files
+      const orderItems = this.readOrderItems(path.join(ordersPath, folderName));
 
-      // Create a HashiCups order
-      new HashiCupsProvider.Order(this, `order-${orderId}`, {
-        alias: `hashicups_order_${orderId}`,
-        coffee: orderContents.coffee,
-        quantity: orderContents.quantity,
+      // Create a HashiCups order for each item
+      orderItems.forEach((item, index) => {
+        new HashiCupsProvider.Order(this, `order-${orderId}-${index}`, {
+          alias: `hashicups_order_${orderId}_${index}`,
+          coffee: item.coffee,
+          quantity: item.quantity,
+        });
       });
     });
 
@@ -45,16 +53,29 @@ export class CoffeeOrderingStack extends TerraformStack {
     });
   }
 
-  private listFiles(dir: string): string[] {
-    const fs = require('fs');
-    return fs.readdirSync(dir);
+  private listOrderFolders(ordersPath: string): string[] {
+    return fs.readdirSync(ordersPath, { withFileTypes: true })
+      .filter((dirent) => dirent.isDirectory())
+      .map((dirent) => dirent.name);
   }
 
-  private readOrderContents(filePath: string): { coffee: string; quantity: number } {
-    const fs = require('fs');
-    const fileContents = fs.readFileSync(filePath, 'utf-8');
-    const [coffee, quantity] = fileContents.split(',');
-    return { coffee, quantity: parseInt(quantity) };
+  private readOrderItems(orderPath: string): OrderItem[] {
+    const orderItems: OrderItem[] = [];
+
+    const files = fs.readdirSync(orderPath, { withFileTypes: true });
+    files.forEach((dirent) => {
+      if (dirent.isFile()) {
+        const itemName = path.basename(dirent.name, path.extname(dirent.name));
+        const itemQuantity = Number(fs.readFileSync(path.join(orderPath, dirent.name), 'utf-8').trim());
+
+        orderItems.push({
+          coffee: itemName,
+          quantity: itemQuantity,
+        });
+      }
+    });
+
+    return orderItems;
   }
 }
 
