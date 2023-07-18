886efc9b812bc5fe74d26aa22615dce819b3a548
Ubuntu <ubuntu@ip-172-31-25-44.ec2.internal>
2023-07-18 17:09:33 +0000

    2nd commit

--- main.ts
+++ main.ts
@@ -1,14 +1,63 @@
-import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { NullProvider, HashiCupsProvider } from '@cdktf/provider-null';
+import { TerraformOutput, TerraformStack, Token } from 'cdktf';
 
-class MyStack extends TerraformStack {
+export class CoffeeOrderingStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // define resources here
+    const orderItemsPath = './order_items';
+
+    // HashiCups provider
+    const hashicupsProvider = new HashiCupsProvider(this, 'hashicups', {
+      alias: 'hashicups',
+    });
+
+    // Null provider
+    const nullProvider = new NullProvider(this, 'null', {
+      alias: 'null',
+    });
+
+    // Orders
+    const orderFiles = this.listFiles(orderItemsPath);
+    orderFiles.forEach((file) => {
+      const orderId = file.replace('.txt', '');
+      const orderFilePath = `${orderItemsPath}/${file}`;
+
+      // Read the file contents
+      const orderContents = this.readOrderContents(orderFilePath);
+
+      // Create a HashiCups order
+      new HashiCupsProvider.Order(this, `order-${orderId}`, {
+        alias: `hashicups_order_${orderId}`,
+        coffee: orderContents.coffee,
+        quantity: orderContents.quantity,
+      });
+    });
+
+    // Outputs
+    new TerraformOutput(this, 'hashicupsProviderAlias', {
+      value: Token.asString(hashicupsProvider.getAlias('hashicups')),
+    });
+    new TerraformOutput(this, 'nullProviderAlias', {
+      value: Token.asString(nullProvider.getAlias('null')),
+    });
+  }
+
+  private listFiles(dir: string): string[] {
+    const fs = require('fs');
+    return fs.readdirSync(dir);
+  }
+
+  private readOrderContents(filePath: string): { coffee: string; quantity: number } {
+    const fs = require('fs');
+    const fileContents = fs.readFileSync(filePath, 'utf-8');
+    const [coffee, quantity] = fileContents.split(',');
+    return { coffee, quantity: parseInt(quantity) };
   }
 }
 
-const app = new App();
-new MyStack(app, "platform-challenge");
+const app = new cdk.App();
+new CoffeeOrderingStack(app, 'CoffeeOrderingStack');
 app.synth();
